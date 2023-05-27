f36a14dd273d0d813acdb89e2b1c8eef1c330d79
melashu <meshu102@outlook.com>
2023-05-27 22:04:23 +0300

    Add tailwind css

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
+gem 'tailwindcss-rails', '~> 2.0', '>= 2.0.29'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameteres, if: :devise_controller?
-
+    protect_from_forgery with: :exception # help to generate, encrypt and descrypt CSRF tooken 
+# protect_from_forgery is defined RequestForgeryProtection concern 
     def configure_permitted_parameteres
     end
 end
