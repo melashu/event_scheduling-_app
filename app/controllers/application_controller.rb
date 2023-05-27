e3a5465a9a822a9f73cb69c505684afcc6a01b5d
melashu <meshu102@outlook.com>
2023-05-28 00:15:31 +0300

    Add strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception # help to generate, encrypt and descrypt CSRF tooken 
 # protect_from_forgery is defined RequestForgeryProtection concern 
     def configure_permitted_parameteres
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name booking_link])
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name booking_link])
     end
 end
