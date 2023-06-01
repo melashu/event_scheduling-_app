9d3007a80993b0e46729d9664934011c96086b68
melashu <meshu102@outlook.com>
2023-06-01 22:26:51 +0300

    Add booking type and booking to dashboard

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,12 @@
 class HomesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def index
+    
   end
 
   def dashboard
+    @booking_type = current_user.booking_types
+    @bookings = Booking.where(booking_type: current_user.booking_type_ids)
   end
 end
