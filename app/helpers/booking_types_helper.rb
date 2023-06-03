0eb7d4317279e90425ca5e6ad257b3f2f96a1fd3
melashu <meshu102@outlook.com>
2023-06-03 22:27:14 +0300

    Update booking type view

--- app/helpers/booking_types_helper.rb
+++ app/helpers/booking_types_helper.rb
@@ -1,2 +1,9 @@
 module BookingTypesHelper
+    def duration(booking_type)
+        if booking_type.duration == 60
+            '1 hr'
+        else
+            booking_type.duration.to_formatted_s + ' mins'
+        end
+    end
 end
