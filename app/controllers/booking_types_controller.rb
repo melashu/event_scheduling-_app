0bb2ca3236ec045dcea8a24f8035997d201b78a3
melashu <meshu102@outlook.com>
2023-06-09 09:45:34 +0300

    Send email notification

--- app/controllers/booking_types_controller.rb
+++ app/controllers/booking_types_controller.rb
@@ -1,5 +1,5 @@
 class BookingTypesController < ApplicationController
-  before_action :set_booking_type, only: %i[ show edit update destroy ]
+  before_action :set_booking_type, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   # GET /booking_types or /booking_types.json
@@ -8,8 +8,7 @@ class BookingTypesController < ApplicationController
   end
 
   # GET /booking_types/1 or /booking_types/1.json
-  def show
-  end
+  def show; end
 
   # GET /booking_types/new
   def new
@@ -17,16 +16,21 @@ class BookingTypesController < ApplicationController
   end
 
   # GET /booking_types/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /booking_types or /booking_types.json
   def create
     @booking_type = current_user.booking_types.new(booking_type_params)
+    # ids = current_user.booking_types.ids
+    # users = Booking.where(booking_type_id: ids).distinct.pluck(:email, :first_name)
+    users = current_user.booking_types.joins(:bookings).distinct.pluck(:email)
 
     respond_to do |format|
       if @booking_type.save
-        format.html { redirect_to root_url, notice: 'Booking type was successfully created.' }
+        users.each do |user|
+          BookingTypeMailer.with(current_user: current_user, email: user[0], first_name: user[1], booking_type: @booking_type).notify.deliver_later
+        end
+        format.html { redirect_to booking_types_url, notice: 'Booking type was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -37,7 +41,7 @@ class BookingTypesController < ApplicationController
   def update
     respond_to do |format|
       if @booking_type.update(booking_type_params)
-        format.html { redirect_to root_url, notice: "Booking type was successfully updated." }
+        format.html { redirect_to root_url, notice: 'Booking type was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -54,13 +58,15 @@ class BookingTypesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking_type
-      @booking_type = current_user.booking_types.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def booking_type_params
-      params.require(:booking_type).permit(:name, :location, :description, :color, :duration, :payement_required, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking_type
+    @booking_type = current_user.booking_types.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_type_params
+    params.require(:booking_type).permit(:name, :location, :description, :color, :start_date, :end_date,
+                                         :payement_required, :price)
+  end
 end
