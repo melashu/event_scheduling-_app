686edd76941f58b12ed30db1a01f9898978d1a28
melashu <meshu102@outlook.com>
2023-06-10 21:50:02 +0300

    Add free_event action controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(booking_link: params[:booking_link])
   end
+
+   def free_event
+    @free_events = BookingType.where(payement_required: false).includes(:user)
+   end
+
 end
