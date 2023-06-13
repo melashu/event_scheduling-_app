6fcc7b13c13116f80000259d64255a1ceec6ce3c
melashu <meshu102@outlook.com>
2023-06-13 10:40:32 +0300

    Add photo

--- app/models/booking_type.rb
+++ app/models/booking_type.rb
@@ -2,6 +2,7 @@ class BookingType < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_rich_text :description
+  has_one_attached :cover_photo
 
   validates :name, :location, :start_date, :end_date, :color, presence: true
 end
