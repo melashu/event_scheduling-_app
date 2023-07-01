870203e3d55e9c002392c32970f0e7d5b52f0d51
melashu <meshu102@outlook.com>
2023-07-01 16:20:29 +0300

    Add paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
    def free_event
-    @free_events = BookingType.where(payement_required: false).includes(:user).order(created_at: :desc)
+    @free_events = BookingType.paginate(page: params[:page]).where(payement_required: false).includes(:user).order(created_at: :desc)
    end
 
 end
